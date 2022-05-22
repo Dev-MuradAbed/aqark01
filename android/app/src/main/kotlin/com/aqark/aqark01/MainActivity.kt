8be7cf29d34147a17ff86f75f86ae556f7ba9258
Murad Abed <Dev-MuradAbed>
2022-05-22 14:50:02 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/aqark/aqark01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aqark.aqark01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/app/bn_screens/categories_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../../app_data.dart';
+import '../../../widget/category_items.dart';
+
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: GridView(
+        padding: const EdgeInsets.all(5),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 250,
+          childAspectRatio: 8/8.5,
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+        ),
+      children: Categories_data.map((categoryData) =>
+          CategoryItem(titles:categoryData.title,images: categoryData.imageUrl,id : categoryData.id ,)).toList(),
+      ),
+    );
+  }
+}
+
